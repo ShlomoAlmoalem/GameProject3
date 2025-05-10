b3ae2bc55cbefb0eeb4f6bfe922a133292281668
ShlomoAlmoalem <shlomo35102@gmail.com>
2025-05-11 02:14:36 +0300

    שינוי

--- main/java/org/example/App.java
+++ main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.add(panel);
+        // טען את תמונת הבאנר
+        Image gameIcon = new ImageIcon(Objects.requireNonNull(App.class.getResource("/images/game_banner.png"))).getImage();
+        // הגדר את האייקון של הפריים
+        frame.setIconImage(gameIcon);
         frame.pack();
         frame.setLocationRelativeTo(null);
         MenuScreen menu = new MenuScreen(frame);
