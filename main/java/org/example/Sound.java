f81d6f8fc7cc9b3d44ad3bacd318403224493bb1
ShlomoAlmoalem <shlomo35102@gmail.com>
2025-05-18 22:37:07 +0300

    שינוי

---
+++ main/java/org/example/Sound.java
@@ -0,0 +1,62 @@
+
+package org.example;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class Sound {
+
+    private AudioInputStream audioInputStream;
+    private Clip clip;
+
+
+
+    public void playSound(String source) {
+        try {
+            // ציין כאן את הנתיב המלא לקובץ הסאונד
+            File soundFile = new File(source);
+            audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+
+
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+
+
+
+
+    public void startPlay(){
+        this.clip.start();
+        this.clip.setFramePosition(0);
+    }
+
+    public void startBackgroundPlay(){
+        this.clip.start();
+    }
+
+    public void loopPlay(){
+        this.clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stopPlay() {
+        this.clip.close();
+
+    }
+
+
+
+    public void restartSound() {
+        if (!clip.isOpen()) {
+            playSound("src/main/java/resources/background_sound.wav");
+        }
+        startPlay();
+        loopPlay();
+    }
+
+}
